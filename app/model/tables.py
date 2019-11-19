4f37d0e73a3fe7b8fcde3a8c0126da3ed9bfc45e
Pedro Sousa <ppls2106@gmail.com>
2019-11-19 02:52:56 -0300

    add relationship

--- app/model/tables.py
+++ app/model/tables.py
@@ -4,13 +4,17 @@ from app import db
 # TODO falta fazer os relacionamentos
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
-    id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    id = db.Column(db.INTEGER, autoincrement=True, primary_key=True, nullable=False)
+    username = db.Column(db.VARCHAR(80), unique=True, nullable=False)
+    password = db.Column(db.VARCHAR(95), nullable=False)
+    name = db.Column(db.VARCHAR(200), nullable=False)
+    email = db.Column(db.VARCHAR(120), unique=True, nullable=False)
+
+    # RELATIONSHIP
+    # One to one
+    pessoa = db.relationship('Pessoa', uselist=False, back_populates='usuario')
 
     def __init__(self, username, password, name, email):
         self.username = username
@@ -38,16 +42,9 @@ class User(db.Model):
     def is_anonymous(self):
         return False
 
+    @property
     def get_id(self):
         return str(self.id)
-
-
-class Unidade(db.Model):
-    __tablename__ = 'unidade'
-
-    id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
-    nome = db.Column(db.VARCHAR(120), nullable=False)
-    endereco_id = db.Column(db.SMALLINT, nullable=False)
 
 
 class Endereco(db.Model):
@@ -62,6 +59,27 @@ class Endereco(db.Model):
     bairro = db.Column(db.VARCHAR(20), nullable=False)
     telefone = db.Column(db.VARCHAR(15), nullable=True)
 
+    # RELATIONSHIP
+    # One to one
+    unidade = db.relationship('Unidade', uselist=False, back_populates='endereco')
+    # One to many
+    pessoas = db.relationship('Pessoa', back_populates='endereco')
+
+
+class Unidade(db.Model):
+    __tablename__ = 'unidade'
+
+    id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
+    nome = db.Column(db.VARCHAR(120), nullable=False)
+    endereco_id = db.Column(db.SMALLINT, db.ForeignKey('endereco.id', name='FK_unidade_endereco'), nullable=False,
+                            unique=True)
+
+    # RELATIONSHIP
+    # One to one
+    endereco = db.relationship('Endereco', back_populates='unidade')
+    # Many to one
+    modalidades = db.relationship('Modalidade', back_populates='unidade')
+
 
 class Modalidade(db.Model):
     __tablename__ = 'modalidade'
@@ -69,9 +87,20 @@ class Modalidade(db.Model):
     id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
     categoria = db.Column(db.VARCHAR(100), nullable=False)
     nivel = db.Column(db.VARCHAR(20), nullable=True)
-    categoria_pai_id = db.Column(db.SMALLINT, nullable=True)
-    unidade_id = db.Column(db.SMALLINT, nullable=False)
-    professor_id = db.Column(db.VARCHAR(10), nullable=True)
+    categoria_pai_id = db.Column(db.SMALLINT, db.ForeignKey('modalidade.id', name='FK_modalidade_categoria'),
+                                 nullable=True)
+    unidade_id = db.Column(db.SMALLINT, db.ForeignKey('unidade.id', name='FK_modalidade_unidade'), nullable=False)
+    professor_id = db.Column(db.SMALLINT, db.ForeignKey('funcionario.id', name='FK_modalidade_professor'),
+                             nullable=True)
+
+    # RELATIONSHIP
+    # Self
+    categorias_pai = db.relationship('Modalidade')
+    # One to many
+    unidade = db.relationship('Unidade', back_populates='modalidades')
+    professor = db.relationship('Funcionario', back_populates='modalidades')
+    # Many to one
+    turmas = db.relationship('Turma', back_populates='modalidade')
 
 
 class Turma(db.Model):
@@ -79,7 +108,11 @@ class Turma(db.Model):
 
     id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
     horario = db.Column(db.TIME, nullable=False)
-    modalidade_id = db.Column(db.SMALLINT, nullable=True)
+    modalidade_id = db.Column(db.SMALLINT, db.ForeignKey('modalidade.id', name='FK_turma_modalidade'), nullable=True)
+
+    # RELATIONSHIP
+    # One to many
+    modalidade = db.relationship('Modalidade', back_populates='turmas')
 
 
 class Pessoa(db.Model):
@@ -90,8 +123,15 @@ class Pessoa(db.Model):
     nome_mae = db.Column(db.VARCHAR(120), nullable=False)
     documento_tipo = db.Column(db.Enum('RG', 'CPF', 'CNH', 'PASSAPORTE', 'OUTRO'), nullable=False)
     documento_num = db.Column(db.VARCHAR(20), nullable=False)
-    endereco_id = db.Column(db.SMALLINT, nullable=False)
-    user_id = db.Column(db.SMALLINT, nullable=False)
+    endereco_id = db.Column(db.SMALLINT, db.ForeignKey('endereco.id', name='FK_pessoa_endereco'), nullable=False)
+    user_id = db.Column(db.SMALLINT, db.ForeignKey('user.id', name='FK_pessoa_user'), nullable=False)
+
+    # RELATIONSHIP
+    # One to one
+    usuario = db.relationship('User', back_populates='pessoa')
+    funcionario = db.relationship('Funcionario', uselist=False, back_populates='pessoa')
+    # Many to one
+    endereco = db.relationship('Endereco', back_populates='pessoas')
 
 
 class Funcionario(db.Model):
@@ -100,8 +140,15 @@ class Funcionario(db.Model):
     id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
     registro = db.Column(db.VARCHAR(10), nullable=False, unique=True)
     ativado = db.Column(db.BOOLEAN, default=True)
-    cargo_id = db.Column(db.SMALLINT, nullable=False)
-    pessoa_id = db.Column(db.SMALLINT, nullable=False)
+    cargo_id = db.Column(db.SMALLINT, db.ForeignKey('cargo.id', name='FK_funcionario_cargo'), nullable=False)
+    pessoa_id = db.Column(db.SMALLINT, db.ForeignKey('pessoa.id', name='FK_funcionario_pessoa'), nullable=False)
+
+    # RELATIONSHIP
+    # One to one
+    cargo = db.relationship('Cargo', back_populates='funcionario')
+    pessoa = db.relationship('Pessoa', back_populates='funcionario')
+    # Many to one
+    modalidades = db.relationship('Modalidade', back_populates='professor')
 
 
 class Aluno(db.Model):
@@ -132,9 +179,13 @@ class Cargo(db.Model):
     id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
     nome = db.Column(db.VARCHAR(20), nullable=False)
 
+    # RELATIONSHIP
+    # One to one
+    funcionario = db.relationship('Funcionario', uselist=False, back_populates='cargo')
+
 
 class AvaliacaoFisica(db.Model):
-    __tablename__ = 'avaliacao_fisica'
+    __tablename__ = 'avaliacaofisica'
 
     id = db.Column(db.SMALLINT, nullable=False, autoincrement=True, primary_key=True)
     avaliador = db.Column(db.SMALLINT, nullable=False)
@@ -188,4 +239,12 @@ class Aparelho(db.Model):
 
 
 # TABELAS INTERMEDIARIAS
-
+linha_ficha = db.Table('linha_ficha',
+                       db.Column('id', db.SMALLINT, nullable=False, autoincrement=True, primary_key=True),
+                       db.Column('ciclo1', db.DECIMAL(precision=5, scale=2)),
+                       db.Column('ciclo2', db.DECIMAL(precision=5, scale=2)),
+                       db.Column('ciclo3', db.DECIMAL(precision=5, scale=2)),
+                       db.Column('ciclo4', db.DECIMAL(precision=5, scale=2)),
+                       db.Column('aparelho_id', db.SMALLINT, db.ForeignKey('aparelho.id')),
+                       db.Column('ficha_id', db.SMALLINT, db.ForeignKey('ficha.id'))
+                       )
