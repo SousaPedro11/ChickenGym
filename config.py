13835202069d7ac81181577d2d07a7c75cec66c9
Pedro Sousa <ppls2106@gmail.com>
2020-10-15 17:34:43 -0300

    prepare to heroku - edit db url

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://aluno:aluno01@localhost:3306/chickengym?charset=utf8mb4'
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///storage.db')
+DATABASE_URI = os.getenv('DATABASE_URI')
+if DATABASE_URI is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+else:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2' + DATABASE_URI
+
 # SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///' + os.path.join(basedir, 'storage.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(16))
