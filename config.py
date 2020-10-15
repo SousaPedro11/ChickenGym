4fba42b5fafcf7fe8bc8405da396bb81b1c7437d
Leal Sistemas <dev@lealsistemas.com>
2020-10-15 16:57:55 -0300

    prepare to heroku

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-import os.path
+import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://aluno:aluno01@localhost:3306/chickengym?charset=utf8mb4'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://iec_desenv:iec_desenv@localhost:3307/chickengym?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///storage.db')
 # SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///' + os.path.join(basedir, 'storage.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = 'Miguel@12345'
+SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(16))
 THREADED = True
--- run.py
+++ run.py
@@ -1,9 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
 from app import manager
 
 # Não excluir
 from app.controller.database_manipulation import usuario
 
 # manager.add_command('runserver', GunicornServer())
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 if __name__ == '__main__':
     manager.run()
